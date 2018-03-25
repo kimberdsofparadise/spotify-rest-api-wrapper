9aa2ccc2b1b1f12839e3ada10df1c27ad0ba22d2
kimberdsofparadise <37759253+kimberdsofparadise@users.noreply.github.com>
2018-03-25 16:40:14 -0700

    Create schema.js
    GraphQL schema

---
+++ schema.js
@@ -0,0 +1,23 @@
+const { makeExecutableSchema } = require("graphql-tools");
+
+const fetch = require("node-fetch");
+
+const gql = String.raw;
+
+// Construct a schema, using GraphQL schema language. See: http://graphql.org/learn/schema/
+const typeDefs = gql`
+  type Query {
+    myFavoriteArtists: [Artist]
+  }
+  type Artist @cacheControl(maxAge: 60) {
+    id: ID
+    name: String
+    relatedArtists: [RelatedArtist]
+	
+  }
+  type RelatedArtist @cacheControl(maxAge: 60) {
+    name: String
+    id: String
+	href: String
+  }
+`;
